2f35c241df7e4b02e532f89cd52dd68d4abffd19
Mac Howe <mac@machowe.com>
2026-05-02 14:08:01 -0400

    get user input

--- app/main.go
+++ app/main.go
@@ -8,4 +8,14 @@ var _ = fmt.Print
 
 func main() {
 	fmt.Print("$ ")
+
+	input := make([]byte, 1024)
+	n, err := fmt.Scan(input)
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		return
+	}
+
+	command := string(input[:n])
+	fmt.Println(command, ": command not found")
 }
