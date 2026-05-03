06b3bd0ef3cae333b8087a6610f8db5f45db2a17
Mac Howe <mac@machowe.com>
2026-05-03 01:14:32 -0400

    loop [skip ci]

--- app/main.go
+++ app/main.go
@@ -10,15 +10,17 @@ import (
 var _ = fmt.Print
 
 func main() {
-	fmt.Print("$ ")
+	for {
+		fmt.Print("$ ")
 
-	reader := bufio.NewReader(os.Stdin)
-	input, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println("Error reading input:", err)
-		return
+		reader := bufio.NewReader(os.Stdin)
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading input:", err)
+			return
+		}
+
+		command := strings.TrimSpace(input)
+		fmt.Printf("%s: command not found\n", command)
 	}
-
-	command := strings.TrimSpace(input)
-	fmt.Printf("%s: command not found\n", command)
 }
